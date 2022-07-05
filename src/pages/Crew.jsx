4ec46f84fd94422025527c996cffefe1ed2c2dd0
DavidXploidz <david.adame.itd@gmail.com>
2022-07-05 15:18:17 -0500

    last part of  desing mobile added

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -9,24 +9,27 @@ const Crew = () => {
 
   return (
     <div className='header-crew'>
-      <div className='contenedor '>
-        <div className='contenedor flex items-center gap-x-5 text-4xl text-left '>
-          <p className='font-bold text-gray-600 mt-96'>02</p>
-          <span className='text-white uppercase tracking-widest mt-96'>meet your crew</span>
-        </div>
-      </div>
+
+      <div className='contenedor flex justify-center items-center gap-x-5 text-2xl text-center md:text-4xl md:text-left md:justify-start '>
+        <p className='font-bold text-gray-600 mt-24 md:mt-96'>02</p>
+        <span className='text-white uppercase tracking-widest mt-24 md:mt-96'>meet your crew</span>
+      </div>   
+ 
       {datos.crew.map((personas, index ) => {
           if(personas.name === valor){
           return (
         
-            <div className='contenedor grid grid-cols-2'>
-              <div>
-                <h4 className='text-gray-400 uppercase mt-56'>{personas.role}</h4>
-                <h3 className='uppercase'>{personas.name}</h3>
-                <p className='w-2/3'>
-                  {personas.bio}
-                </p>
-                <div className='contenedor flex gap-x-10 '>
+            <div className='contenedor flex flex-col-reverse md:grid md:grid-cols-2 '> {/**grid grid-cols-1 md:grid md:grid-cols-2 */}
+              <div className='flex flex-col-reverse md:block'>
+                <div>
+                  <h4 className=' text-gray-400 uppercase  md:mt-56'>{personas.role}</h4>
+                  <h3 className='uppercase'>{personas.name}</h3>
+                  <p className='md:w-2/3'>
+                    {personas.bio}
+                  </p>
+                </div>
+                
+                <div className='contenedor flex justify-center  gap-x-10 md:justify-start '>
                   {datos.crew.map ((boton, index) => (
                     <button 
                     onClick={e => {
@@ -34,13 +37,13 @@ const Crew = () => {
                       setValue(index)
                     }}
                     value={boton.name}
-                    className={`rounded-full h-5 w-5 mt-40 hover:bg-gray-400 ${index === value ? 'bg-white': 'bg-gray-600 '}`}>
+                    className={`rounded-full h-5 w-5 my-20 md:mt-40 hover:bg-gray-400 ${index === value ? 'bg-white': 'bg-gray-600 '}`}>
 
                     </button>
                   ))}
                 </div>
               </div>
-              <img src={personas.images.webp} alt={`Imagen de crew`}  className='grid self-end place-self-center maximo'/>
+              <img src={personas.images.webp} alt={`Imagen de crew`}  className='h-96 md:h-auto grid self-center md:self-end place-self-center maximo mt-5 md:mt-0'/>
               {}
             </div>
             )
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -10,17 +10,17 @@ const Technology = () => {
   return (
     <div className='header-technology'>
       <div>
-        <div className='contenedor flex items-center gap-x-5 text-4xl text-left '>
-            <p className='font-bold text-gray-600 mt-96'>03</p>
-          <span className='text-white uppercase tracking-widest mt-96'>space launch 101</span>
-        </div>
+      <div className='contenedor flex justify-center items-center gap-x-5 text-2xl text-center md:text-4xl md:text-left md:justify-start '>
+        <p className='font-bold text-gray-600 mt-24 md:mt-96'>03</p>
+        <span className='text-white uppercase tracking-widest mt-24 md:mt-96'>space launch 001</span>
+      </div>
         {datos.technology.map((cohetes, index) => {
           if(cohetes.name === valor){
             return (
               <>
-              <div className='flex justify-between items-center gap-x-10'>
-                <div className='flex gap-x-20 '>
-                  <div className='ml-40 flex flex-col gap-5'>
+              <div className='flex flex-col-reverse md:flex-row md:justify-between md:items-center gap-x-10'>
+                <div className='flex flex-col md:flex-row gap-x-20 '>
+                  <div className='flex flex-row mx-auto md:ml-96 md:flex-col gap-5 mt-10 md:mt-0'>
                     {datos.technology.map((boton, index) => (
                       <button 
                       value={boton.name}
@@ -29,19 +29,17 @@ const Technology = () => {
                       <p className={`mx-auto cursor-not-allowed ${index === value ? 'text-Azul' : 'text-white'}`}>{index + 1}</p>
                       </button>
                     ))}
-                    
-                 
-                  
+
                   </div>
-                  <div>
-                    <h5 className='text-base'>the terminology...</h5>
-                    <h3 className='uppercase'>{cohetes.name}</h3>
-                    <p className='w-2/3'>
+                  <div className='mt-10 md:mt-0'>
+                    <h5 className='text-2xl'>the terminology...</h5>
+                    <h3 className='uppercase my-3'>{cohetes.name}</h3>
+                    <p className='w-full md:w-2/3'>
                       {cohetes.description}
                     </p>
                   </div>
                 </div>
-                <img src={cohetes.images.landscape} alt={`Imagen de cohete ${cohetes.images.portrait}`} className="cohetes-img" />
+                <img src={cohetes.images.landscape} alt={`Imagen de cohete ${cohetes.name}`} className="cohetes-img h-42 mt-10 md:mt-0" />
               </div>
                 
               </>
